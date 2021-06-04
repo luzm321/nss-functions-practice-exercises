436aa9610b9a4d8ea2b4778caada6a113aba2fd2
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-06-04 17:51:09 -0500

    completed more function exercises

--- practice.js
+++ practice.js
@@ -70,35 +70,106 @@
 //     console.log(evenOrOdd(number));
 // };
 
-//-------------------Double Functions Exercise:------------------
+//-------------------Double Functions Exercise (Using filter method on a function to achieve goal):------------------
 
-const words = [
-    "The", "killing", "complex", "houses",
-    "married", "kittens", "and", "single",
-    "soldiers", "and", "their", "kleptomaniacal",
-    "families"
-];
+// const words = [
+//     "The", "killing", "complex", "houses",
+//     "married", "kittens", "and", "single",
+//     "soldiers", "and", "their", "kleptomaniacal",
+//     "families"
+// ];
 
 /*
-    Function to filter out `k` words
-    (Google 'javascript startswith' to get started)
+    Function that filters out `k` words and also builds a single string/sentence from the array
  */
 
-/*
-    Function to build a single string from the array
-    (Google 'javascript combine all items in array' to get started)
- */
+// const filterKWords = () => {
+//     const filteredWords = words.filter(word => {
+//         if (word.startsWith("k")) {
+//             //startsWith() method returns true if meets condition so don't need to do anything in if statement
+//         } else {
+//             return word; // words with the "k" filtered out
+//         };
+//     });
+//     // console.log(filteredWords);
+//     const filteredSentence = filteredWords.join(" ");
+//     console.log(`${filteredSentence}.`);
+// };
+
+// filterKWords();
+
+//----------------------You Can Tune A Piano, But You Can't...Exercise:--------------------------------------
+
+// const catchingTunaProbability = () => {
+//     if (Math.round(Math.random() * 2) <= 1) {
+//         console.log("Sven hooked a tuna! :)");
+//         return true;
+//     } else {
+//         console.log("Sven came up empty-handed. :(");
+//         return false;
+//     };
+// };
+
+// console.log(catchingTunaProbability());
+
+//----------------------Fast Food (Multiple Parameters) Exercise:---------------------------------------------
+
+// const orderMeal = (sandwich, side, drink, dessert) => {
+//     const myMeal = {
+//         mySandwich: sandwich,
+//         mySide: side,
+//         myDrink: drink,
+//         myDessert: dessert
+//     };
+
+//     console.log(`I would like to order a ${myMeal.mySandwich} sandwich, ${myMeal.mySide}, ${myMeal.myDrink}, and ${myMeal.myDessert} for lunch as a takeout.`);
+//     return myMeal;
+// };
+
+// const takeOutBag = orderMeal("roast beef", "sweet potato fries", "taro boba", "strawberry cheesecake");
+// console.log(takeOutBag);
+
+//----------------------------Same Chores, Different Days Exercises(Higher Order Functions):-------------------------------------------
+
+//*Number function example*:
+
+//Individual functions:
+// const twice = number => number * 2;
+// console.log(twice(2));
+
+// const timesFive = number => number * 5;
+// console.log(timesFive(2));
+
+// const square = number => number * number;
+// console.log(square(2));
+
+// const cube = number => Math.pow(number, 3);
+// console.log(cube(2));
+
+// *Higher Order Function*:
+
+// const bigify = (number, operationFunction) => {
+//     const result = operationFunction(number);
+//     return result;
+// };
+
+// *Performing all four operations on the same number by invoking bigify()*:
+
+// const number = 2;
+
+// const multiplyByTwo = bigify(number, twice);
+// console.log(multiplyByTwo);
+
+// const multiplyByFive = bigify(number, timesFive);
+// console.log(multiplyByFive);
+
+// const squaredValue = bigify(number, square);
+// console.log(squaredValue);
+
+// const cubedValue = bigify(number, cube);
+// console.log(cubedValue);
+
+// *Defining the Chores example*:
 
 
-/*
-    Invoke the filtering function and store its return value
- */
 
-
-/*
-    Invoke the string building function, and pass the
-    return value of the previous function as an argument
-*/
-
-
-console.log()
