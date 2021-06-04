848cfae9f3b276bf1b093dd494a45a7358faf2a5
Luz Angelique Madrazo <luzm321@yahoo.com>
2021-06-04 14:41:33 -0500

    initial commit, completed certain function practice exercises

---
+++ practice.js
@@ -0,0 +1,104 @@
+//---------------- Age in Dog Years Exercise:----------------
+
+// const calculateAgeInDogYears = (ageOfPerson) => {
+//     const ageInDogYears = ageOfPerson / 7;
+
+//     return ageInDogYears;
+// };
+
+// const dogAge = calculateAgeInDogYears(45).toFixed(2);
+// console.log(dogAge);
+
+//-----------------Best in Show Exercise:--------------------
+
+// const displayFavoritePet = (favoritePet) => {
+//     if (favoritePet === "meow") {
+//         return "I like cats.";
+//     };
+
+//     return `My favorite dog breed is ${favoritePet}.`;
+// };
+
+// const myFavoritePet = displayFavoritePet("maltese");
+// console.log("When it comes to pets, " + myFavoritePet);
+
+// const myFavoritePet = displayFavoritePet("meow");
+// console.log("When it comes to pets, " + myFavoritePet);
+
+//----------------Addition Exercise:--------------------------
+
+// const add = (num1, num2, num3) => {
+//     const sum = num1 + num2 + num3;
+//     return sum;
+// };
+
+// const addedNumbers = add(1, 2, 3);
+
+// console.log(addedNumbers);
+
+//-----------------Self-Driving Cars Exercise:-----------------
+
+// const go = (direction, speed) => {
+//     if (speed > 75) {
+//         console.log(`The car is moving ${direction} at ${speed} mph. SLOW DOWN!`);
+//     }
+//     console.log(`The car is moving ${direction} at ${speed} mph.`);
+// };
+
+
+// console.log(go("forwards", 45));
+// console.log(go("backwards", 8));
+// console.log(go("in circles", 12));
+// console.log(go("forward", 82));
+
+//-----------------Evens or Odds Exercise:-----------------------
+
+// const evenOrOdd = (number) => {
+//     if (number % 2 === 0) {
+//         return "Even";
+//     } else {
+//         return "Odd";
+//     };
+// };
+
+// console.log(evenOrOdd(4));
+// console.log(evenOrOdd(5));
+
+// const numbersArray = [0, 1, 2, 3, 4, 5];
+
+// for (const number of numbersArray) {
+//     console.log(evenOrOdd(number));
+// };
+
+//-------------------Double Functions Exercise:------------------
+
+const words = [
+    "The", "killing", "complex", "houses",
+    "married", "kittens", "and", "single",
+    "soldiers", "and", "their", "kleptomaniacal",
+    "families"
+];
+
+/*
+    Function to filter out `k` words
+    (Google 'javascript startswith' to get started)
+ */
+
+/*
+    Function to build a single string from the array
+    (Google 'javascript combine all items in array' to get started)
+ */
+
+
+/*
+    Invoke the filtering function and store its return value
+ */
+
+
+/*
+    Invoke the string building function, and pass the
+    return value of the previous function as an argument
+*/
+
+
+console.log()
